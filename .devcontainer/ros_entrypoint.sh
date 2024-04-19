63af2dcf4b578cb33f5e2700f2d9e2e2a827d1a7
Scott Hickmann <scotthickmann21@gmail.com>
2024-04-18 22:48:35 -0700

    Add simulator

--- .devcontainer/ros_entrypoint.sh
+++ .devcontainer/ros_entrypoint.sh
@@ -4,10 +4,8 @@ set -e
 
 # setup ros2 environment
 source /opt/ros/"$ROS_DISTRO"/setup.bash --
-source ~/ros2_ws/install/setup.bash --
 
 # add sourcing to .bashrc
 echo "source '/opt/ros/$ROS_DISTRO/setup.bash'" >> ~/.bashrc
-echo "source '~/ros2_ws/install/setup.bash'" >> ~/.bashrc
 
 exec "$@"
