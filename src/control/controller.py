f9345b68b87910c15931a24f14c3787dbbea4079
aliahmad810 <hiimaliahmad@gmail.com>
2024-05-07 21:48:28 -0700

    Framework for controller and pursuit

--- src/control/controller.py
+++ src/control/controller.py
@@ -1,68 +1,7 @@
 import rclpy
+from rclpy.node import Node
 import threading
 import numpy as np
-
-
-# class PIDController :
-
-#     def __init__(self, kP, kI, kD, p_start_i) :
-#         """
-#         Initialization for PIDController class
-
-#         :param kP: Error gain
-#         :param kI: Integral gain
-#         :param kD: Derivative gain
-#         :param p_start_i: Error at which to start integral term
-#         """
-#         self._kP = kP
-#         self._kI = kI
-#         self._kD = kD
-#         self._p_start_i = p_start_i
-#         self._integral = 0
-#         self._prev_error = 0
-#         self.running = False
-#         self.lock = threading.Lock()
-
-    
-#     def update(self, desired, state, dt) :
-#         """
-#         Updates the controller and returns new output power
-
-#         :param desired: The desired state of the robot
-#         :param state: Robot's current state
-#         :param dt: Time step from last control loop, in seconds
-#         :return: Control signal
-#         """
-#         error = desired - state
-#         derivative = (error - self._prev_error) / dt
-#         # We wait to incorporate integral term to avoid windup
-#         if(error <= self._p_start_i) :
-#             self._integral += error * dt
-#         output = error * self._kP + self._integral * self._kI + derivative * self._kD
-#         # Current error becomes the previous in the next loop
-#         self._prev_error = error
-#         return output
-
-#     def reset(self) :
-#         """
-#         Resets the controller by setting the integral and previous error to 0
-#         """
-#         self._integral = 0
-#         self._prev_error = 0
-
-#     def start(self, feedback_source) :
-#         """
-#         Starts the rospy node and PID controller thread
-#         """
-#         rclpy.spin()
-#         self.running = True
-
-#     def stop(self):
-#         """
-#         Stops the rospy node and shuts down the controller thread
-#         """
-#         self.running = False
-
 
 class nPIDController :
     """
@@ -115,3 +54,18 @@ class nPIDController :
         # Current error becomes the previous in the next loop
         self._prev_error = error
         return output
+    
+class PIDSubscriber(Node) :
+
+    def __init__(self) :
+        return
+    
+class PIDPublisher(Node) :
+
+    def __init__(self) :
+        return
+
+class PIDController :
+
+    def __init__(self) :
+        return
---
+++ src/control/pursuit.py
@@ -0,0 +1,18 @@
+import rclpy
+from rclpy.node import Node
+import numpy as np
+
+def findClosest() :
+    return
+
+def circleIntersect() :
+    return
+
+def lookaheadPoint() :
+    return
+
+def findLookaheadCurvature() :
+    return
+
+def follow() :
+    return
