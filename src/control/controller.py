4a64a0f265949d2638c4219721ff6fd166e31d40
aliahmad810 <hiimaliahmad@gmail.com>
2024-05-09 16:07:18 -0700

    Working on ROS2 controller implementation

--- src/control/controller.py
+++ src/control/controller.py
@@ -1,71 +1,88 @@
 import rclpy
 from rclpy.node import Node
-import threading
+from std_msgs.msg import Float64MultiArray
 import numpy as np
 
-class nPIDController :
-    """
-    PID Controller class that works in n-dimensions
-    """
-
-    def __init__(self, kP, kI, kD, p_start_i) :
-        """
-        Initialization for nPIDController class
-
-        :param kP: Error gains; np matrix
-        :param kI: Integral gains; np matrix
-        :param kD: Derivative gains; np matrix
-        :param p_start_i: Error at which to start integral term; np matrix
-        """
-        self.kP_ = kP
-        self.kI_ = kI
-        self.kD_ = kD
-        self.p_start_i_ = p_start_i
-        self.dimension_ = kP.ndim()
-        self.integral_ = np.zeros(self.dimension_)
-        self.prev_error_ = np.zeros(self.dimension_)
-        self.running = False
-        self.lock = threading.Lock()
-
-    def reset(self) :
-        """
-        Resets the controller by setting the integral and previous error arrays to 0
-        """
-        self.integral_ = np.zeros(self.dimension_)
-        self.integral_ = np.zeros(self.dimension_)
-
-    def update(self, desired, state, dt) :
-        """
-        Updates the controller and returns new output power
-
-        :param desired: The desired state of the robot; np array
-        :param state: Robot's current state; np array
-        :param dt: Time step from last control loop, in seconds
-        :return: Control signal; np array
-        """
-        error = desired - state
-        derivative = (error - self._prev_error) / dt
-        # to account for n-dimensions, loop through all values in sub's state
-        for i in range(self.dimension_) :
-            # We wait to incorporate integral term to avoid windup
-            if(error[i] <= self._p_start_i[i][i]) :
-                self._integral[i] += error[i] * dt
-        output = self.kP_ @ error  + self.kI_ @ self.integral + self.kD_ @ derivative
-        # Current error becomes the previous in the next loop
-        self._prev_error = error
-        return output
-    
-class PIDSubscriber(Node) :
-
-    def __init__(self) :
-        return
-    
-class PIDPublisher(Node) :
-
-    def __init__(self) :
-        return
 
 class PIDController :
 
+    def __init__(self, kP, kD, kI, p_start_i) :
+        self.kP_ = kP
+        self.kD_ = kD
+        self.kI_ = kI
+
+        self.p_start_i_ = p_start_i
+
+        self.dimension_ = kP.ndim()
+        self.integral = np.zeros(self.dimension_)
+        self.prev_error = np.zeros(self.dimension_)
+
+    def reset(self) :
+        self.integral = np.zeros(self.dimension_)
+        self.prev_error = np.zeros(self.dimension_)
+
+    def get_state(self, msg) :
+        self.state = msg
+
+    def update(self, desired, dt) :
+        error = desired - self.state
+        derivative = (error - self.prev_error_) / dt
+
+        # to account for n-dimensions, loop through all values in sub's state vector
+        for i in range(self.dimension_) :
+            # We wait to incorporate integral term to avoid windup
+            if(error[i] <= self.p_start_i_[i][i]) :
+                self.integral[i] += error[i] * dt
+        
+        output = (self.kP_ @ error) + (self.kD_ @ derivative) + (self.kI_ @ self.integral)
+
+        # current error becomes previous error in the next loop
+        self.prev_error = error
+
+        return output
+    
+    
+class PIDSubscriber(Node) :
+
+    def __init__(self, controller) :
+        # Pass name of the subscriber
+        super().__init__('pid_subscriber')
+
+        self.subscription = self.create_subscription(
+            np.array(), 
+            'pose', 
+            self.pid_callback,
+            10
+        )
+
+    def pid_callback(self, msg) :
+        pose = np.array(msg.data)
+        self.get_logger().info('Received pose: %s' % pose)
+        # Call the PID controller and publish the output
+        # pid_controller.update(pose)
+
+    
+
+class PIDPublisher(Node) :
+
     def __init__(self) :
-        return
+        super().__init__('pid_publisher')
+        self.initialized_ = False
+
+        self.output_pub_ = self.create_publisher()
+
+def main(args=None) :
+    rclpy.init(args=args)
+
+    # Initialize PID gains
+    kP = np.diag(np.array([0, 0, 0, 0, 0, 0]))
+    kD = np.diag(np.array([0, 0, 0, 0, 0, 0]))
+    kI = np.diag(np.array([0, 0, 0, 0, 0, 0]))
+    p_start_i = np.diag(np.array([0, 0, 0, 0, 0, 0]))
+
+    global controller 
+    controller = PIDController(kP, kD, kI, p_start_i)
+
+
+if __name__ == '__main__' :
+    main()
