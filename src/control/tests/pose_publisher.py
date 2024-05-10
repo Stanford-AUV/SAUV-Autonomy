47162f2dd17c8f7ce0015b2c6bc7fac7b1649382
TomoCoder123 <thomasxdeng@gmail.com>
2024-05-10 00:05:46 -0700

    test package work in progress

---
+++ src/control/tests/pose_publisher.py
@@ -0,0 +1,30 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64MultiArray
+import numpy as np
+
+class PosePublisher(Node) :
+
+    def __init__(self) :
+        super().__init__('output_publisher')
+        self.publisher_ = self.create_publisher(
+            Float64MultiArray,
+            'pose',
+            10
+        )
+        timer_period = 10 # seconds
+        self.timer = self.create_timer(timer_period, self.publish_output)
+
+    def publish_output(self) :
+            msg = Float64MultiArray()
+            msg.data = [[0,0,0,0,0,0]]
+            self.publisher_publish(msg)
+            self.get_logger().info('Publishing output: %s' % msg.data)
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = PosePublisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
+if __name__ == '__main__':
+     main()
