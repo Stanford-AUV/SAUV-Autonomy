f5c97b24486bc7cf25b5065019295f97864d06ad
aliahmad810 <hiimaliahmad@gmail.com>
2024-05-09 21:20:01 -0700

    [WIP] Controller Implementation

--- src/control/controller.py
+++ src/control/controller.py
@@ -20,12 +20,16 @@ class PIDController :
     def reset(self) :
         self.integral = np.zeros(self.dimension_)
         self.prev_error = np.zeros(self.dimension_)
+        self.desired = self.state
 
     def set_state(self, msg) :
         self.state = msg
 
-    def update(self, desired, dt) :
-        error = desired - self.state
+    def set_desired(self, msg) :
+        self.desired = msg
+
+    def update(self, dt) :
+        error = self.desired - self.state
         derivative = (error - self.prev_error_) / dt
 
         # to account for n-dimensions, loop through all values in sub's state vector
@@ -39,27 +43,44 @@ class PIDController :
         # current error becomes previous error in the next loop
         self.prev_error = error
 
-        return output
+        return output.astype(float64)
     
     
-class PIDSubscriber(Node) :
+class PoseSubscriber(Node) :
 
     def __init__(self, controller) :
         super().__init__('pose_subscriber')
 
         self.subscription_ = self.create_subscription(
-            np.array(), 
-            'pose', 
-            self.pid_callback,
-            10
+            Float64MultiArray, 
+            'state', 
+            self.pose_callback,
+            10 # Temporary queue size
         )
 
-    def pid_callback(self, msg) :
+    def pose_callback(self, msg) :
         pose = np.array(msg.data)
         self.get_logger().info('Received pose: %s' % pose)
         controller.set_state(pose)
 
+
+class DesiredSubscriber(Node) :
     
+    def __init__(self) :
+        super().__init__('desired_subscriber')
+
+        self.subscription_ = self.create_subscription(
+            Float64MultiArray,
+            'desired_pos',
+            self.desired_callback,
+            10 # Temporary queue size
+        )
+
+    def desired_callback(self, msg) :
+        desired = np.array(msg.data)
+        self.get_logger().info('Received desired: %s' % desired)
+        controller.set_desired(desired)
+
 
 class PIDPublisher(Node) :
 
@@ -67,15 +88,17 @@ class PIDPublisher(Node) :
         super().__init__('output_publisher')
         self.publisher_ = self.create_publisher(
             Float64MultiArray,
-            'controller_output',
+            'desired_wrench',
             10
         )
+        timer_period = .01 # seconds
+        self.timer = self.create_timer(timer_period, self.publish_output)
 
-        def publish_output(self, output) :
+        def publish_output(self) :
             msg = Float64MultiArray()
-            msg.data = output
+            msg.data = controller.update()
             self.publisher_.publish(msg)
-            self.get_logger().info('Publishing output: %s' % output)
+            self.get_logger().info('Publishing output: %s' % msg.data)
 
 
 def main(args=None) :
@@ -92,16 +115,20 @@ def main(args=None) :
     controller = PIDController(kP, kD, kI, p_start_i)
 
     # Create pose subscriber and output publisher
-    pose_subscriber = PIDSubscriber()
+    pose_subscriber = PoseSubscriber()
+    desired_subscriber = DesiredSubscriber()
     output_publisher = PIDPublisher()
 
     try :
         rclpy.spin(pose_subscriber)
+        rclpy.spin(desired_subscriber)
         rclpy.spin(output_publisher)
     except KeyboardInterrupt :
         pass
-
+    
+    # Cleanup
     pose_subscriber.destroy_node()
+    desired_subscriber.destroy_node()
     output_publisher.destroy_node()
     
     rclpy.shutdown()
