6ac82b852b2d973670acb9ebeddbb4509abeb6b7
aliahmad810 <hiimaliahmad@gmail.com>
2024-05-14 18:11:52 -0700

    Controller rewrite

--- src/control/controller.py
+++ src/control/controller.py
@@ -6,170 +6,138 @@ from std_srvs.srv import Empty
 import numpy as np
 import threading
 
-# Prune state.msg to just the position and quaternion (i.e. 'pose')
-def prune_state(state) :
-    pose = np.array([
+def get_pose_from_state(state) :
+    """
+    Prune state.msg to just the position and quaternion.
+
+    :param state: Float64MultiArray representing position, quaternion, velocity, & acceleration.
+    :return pose: x, y, z, roll, pitch, yaw.
+    """
+    return np.array([
         state.position.x,
         state.position.y,
         state.position.z,
-        state.orientation.x, # roll
-        state.orientation.y, # pitch
-        state.orientation.z  # yaw
+        state.orientation.x,
+        state.orientation.y,
+        state.orientation.z
     ])
 
-def clamp(value, limits) :
-    lower, upper = limits
-    if value is None:
-        return None
-    elif(upper is not None) and (value > upper) :
-        return upper
-    elif(lower is not None) and (value < lower) :
-        return lower
-    return value
+def clamp(value, limits)
+    """
+    Clamp a value to bounds
+
+    :param value: The value to be clamped.
+    :param limits: List containing the bounds
+    """
 
 
-class PIDController(Node) :
-    
-    def __init__(self, kP, kD, kI, p_start_i) :
-        # Gains are a diagonal matrix
-        self.kP_ = kP
-        self.kD_ = kD
-        self.kI_ = kI
-        self.p_start_i = p_start_i
+class Controller(Node) :
+    def __init__(self, p_values, i_values, d_values, start_i_values) :
+        """
+        Initialize Controller object with PID parameters for position and orientation.
 
-        self.dimension_ = 6 # dim(matrix)
+        :param p_values: Diagonal matrix of P gains for the controller [pX, pY, pZ, pRoll, pPitch, pYaw].
+        :param i_values: Diagonal matrix of I gains for the controller [iX, iY, iZ, iRoll, iPitch, iYaw].
+        :param d_values: Diagonal matrix of D gains for the controller [dX, dY, dZ, dRoll, dPitch, dYaw].
+        :param start_i_values: Diagonal matrix of values to start integral for the controller [startX, startY, startZ, startRoll, startPitch, startYaw].
+        """
+        self.kP_ = p_values
+        self.kI_ = i_values
+        self.kD_ = d_values
+        self.start_I_ = start_i_values
 
-        # Initialize controller iterables
-        # At first time step everything is 0
-        self.integral = np.zeros(self.dimension_)
-        self.prev_error = np.zeros(self.dimension_)
+        self.dim_ = 6   # 6 DOF
 
-        # Declare current and desired pose variables
-        self.pose = np.zeros(self.dimension_)
-        self.desired = np.zeros(self.dimension_)
+        # Initialize mutables; everything is 0 at t_0
+        self.integral = np.zeros(self.dim_)
+        self.derivative = np.zeros(self.dim_)
+        self.desired = np.zeros(self.dim_)
 
-        # Initialize state subscriber
         super().__init__('state_subscriber')
-        self.subscription_ = self.create_subscription(
-            Float64MultiArray, 
-            'state', 
-            self.pose_callback,
-            10 # Temporary queue size
-        )
-
-        # Initialize desired state subscriber
+        self.state_subscription_ = self.create_subscription(Float64MultiArray, 'state', self.pose_callback, 10)
         super().__init__('desired_subscriber')
-        self.subscription_ = self.create_subscription(
-            Float64MultiArray,
-            'desired_pos',
-            self.desired_callback,
-            10 # Temporary queue size
-        )
-
-        # Initialize wrench publisher
+        self.desired_subscription_ = self.create_subscription(Float64MultiArray, 'desired_pos', self.desired_callback, 10)
         super().__init__('output_publisher')
-        self.publisher_ = self.create_publisher(
-            Float64MultiArray,
-            'desired_wrench',
-            10
-        )
-        self.timer_period_ = .01 # seconds (10ms / 100Hz)
-        self.timer = self.create_timer(self.timer_period_, self.publish_output)
-
-        # Create mutex
-        self.running = False
-        self.lock = threading.Lock()
-        
-        # Declare parameters and allow for runtime reconfiguration
-        self.declare_parameters(
-            namespace = '', 
-            parameters = [
-                ('kP', self.kP_),
-                ('kD', self.kD_),
-                ('kI', self.kI_),
-                ('p_start_i', self.p_start_i)
-            ]
-        )
+        self.output_publisher_ = self.create_publisher(Float64MultiArray, '')
+        set.timer_period_ = .01   # seconds (100 Hz or 10 ms update cycle)
 
         # Services
-        self.reset_service = self.create_service(Empty, 'reset_controller', self.handle_reset)
+        self.reset_service = self.create_service(Empty, 'reset_controller', self.reset)
 
-        # Add a parameter callback
-        self.add_on_set_parameters_callback(self.parameters_callback)
-
-    def parameters_callback(self, params) :
-        for param in params :
-            if param.name == 'kP' :
-                self.kP_ = param.value
-            if param.name == 'kD' :
-                self.kD_ = param.value
-            if param.name == 'kI' :
-                self.kI_ = param.value
-            if param.name == 'p_start_i' :
-                self.p_start_i_ = param.value
+        # Create mutex to prevent race conditions
+        self.running = False
+        self.lock = threading.Lock()
 
     def pose_callback(self, msg) :
+        """Get our current pose from a topic."""
         with self.lock :
-            self.pose = np.array(prune_state(msg.data))
+            self.pose = np.array(get_pose_from_state(msg.data))
             self.get_logger().info('Received pose: %s' % self.pose)
-        
+
     def desired_callback(self, msg) :
+        """Get the desired pose from a topic."""
         with self.lock :
-            self.desired = np.array(prune_state(msg.data))
+            self.desired = np.array(get_pose_from_state_state(msg.data))
             self.get_logger().info('Received desired pose: %s' % self.desired)
 
-    def update_wrench(self) :
+    def update(self) :
+        """Update the controller with the current state and publish to a topic"""
         error = self.desired - self.pose
-        derivative = error - self.prev_error
+        self.derivative = error = self.prev_error
 
-        # Pose represented as a vector, so we loop through elements
-        for i in range(self.dimension_) :
-            # Delay integral term to avoid integral windup
-            if(error[i] <= self.p_start_i[i][i]) :
+        # Pose is represented as a vector, so we loop through all elements to incorporate integral term
+        for i in range(self.dim_) :
+            # Delay integral term to avoid integral wind-up
+            if error[i] <= self.start_I_[i][i] :
                 self.integral[i] += error[i]
 
-        # Wrench is a linear combination of error, derivative, and integral vectors
-        wrench = (self.kP_ @ error) + (self.kD_ @ derivative) + (self.kI_ @ self.integral) 
+        # Output is a linear combination of error, derivative, and integral vectors
+        output = (self.kP_ @ error) + (self.kI @ self.integral) + (self.kD_ @ self.derivative)
+
+        # Publish a list of control outputs:
+        # [force_x, force_y, force_z, torque_roll, torque_pitch, torque_yaw]
+        msg = Float64MultiArray(data = output)
+        self.output_publisher_.publish(msg)
+        self.get_logger().info('Publishing output %s' % msg.data)
 
         # Current error becomes previous error in the next time step
         self.prev_error = error
-        
-        # Publish the wrench
-        msg = Float64MultiArray(data = wrench)
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing output %s' % msg.data)
 
-    def reset(self) :
+    def reset(self, request, response) :
+        """
+        Reset the controller settings.
+
+        :param request: The request message sent by the service (if any).
+        :param response: The response message sent to the service (if any).
+        :return response: The response, indicating that reset is successful.
+        """
+        self.get_logger().info('Resetting controler settings.')
         with self.lock :
             self.integral = 0
+            self.derivative = 0
             self.prev_error = 0
-
-    def handle_reset(self, request, response) :
-        self.get_logger().info('Resetting controller settings.')
-        self.reset()
-        return response
-
-
+            return response
+        
 def main(args=none) :
     rclpy.init(args=args)
 
-    # Initialize PID gains
+    # Initialize controller gains
     #                      x, y, z, r, p, y
     kP = np.diag(np.array([1, 1, 1, 1, 1, 1]))
     kD = np.diag(np.array([0, 0, 0, 0, 0, 0]))
     kI = np.diag(np.array([0, 0, 0, 0, 0, 0]))
-    p_start_i = np.diag(np.array([0, 0, 0, 0, 0, 0]))
+    start_I = np.diag(np.array([0, 0, 0, 0, 0, 0]))
 
-    controller_node = PIDController(kP, kD, kI, p_start_i)
+    controller_node = Controller(kP, kD, kI, start_I)
 
-    try :
+    try:
         rclpy.spin(controller_node)
-    except KeyboardInterrupt :
+    except KeyboardInterrupt:
         pass
-    
+
     # Cleanup
     controller_node.destroy_node()
-    
+
     rclpy.shutdown()
 
 if __name__ == '__main__' :
