3a16a871942831124eb73a9d822d99b634a94aac
selenasun1618 <selenasun02@gmail.com>
2024-05-28 21:58:46 -0700

    wrote subscriber to desired_wrench

--- src/control/thruster_manager.py
+++ src/control/thruster_manager.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 import numpy as np
 
 from std_msgs.msg import Int16, Float64
-from msgs.msg import Wrench
+import geometry_msgs.msg as geometry_msgs
 from control.force_to_pwm import total_force_to_individual_thrusts, thrusts_to_pwm
 from control.utils import wrench_to_np
 
@@ -14,9 +14,17 @@ class ThrusterManager(Node):
         super().__init__("thruster_manager")
 
         self._thruster_ids = [f"thruster_{i}" for i in range(1, 9)]
-
         self.wrench = np.zeros(6)
+        
+        # Subscribe to 'desired_wrench'
+        self.sub_desired_wrench = self.create_subscription(
+                geometry_msgs.WrenchStamped,
+                'topic,',
+                self.wrench_callback,
+                10)
+        self.subscription # Prevent unused variable warning
 
+        # Publish to 'thrusters/{thruster}/pwm'
         self._pwm_pubs = []
         self._thrust_pubs = []
         for thruster in self._thruster_ids:
@@ -30,7 +38,7 @@ class ThrusterManager(Node):
             Wrench, "desired_wrench", self.wrench_callback, 10
         )
 
-        timer_period = 0.01  # TODO: Don't hardcode this
+        timer_period = 0.01  # Publish rate to desired_wrench
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
