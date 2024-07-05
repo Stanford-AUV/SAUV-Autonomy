3b7955da7eaf3a0e26ce828397078ecb32fe08d1
selenasun1618 <selenasun02@gmail.com>
2024-07-05 12:15:52 -0700

    working depth/IMU synchronization, tested, DVL untested

--- src/firmware/firmware/sync_filter.py
+++ src/firmware/firmware/sync_filter.py
@@ -3,10 +3,11 @@ import ms5837
 import math
 
 from rclpy.node import Node
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Bool
 from msgs.msg import (
     SensorSync as SyncMsg, 
-    MTi200Data as Imu 
+    MTi200Data as Imu,
+    DepthData as Depth 
 )
 from geometry_msgs.msg import Twist
 from message_filters import Subscriber, ApproximateTimeSynchronizer
@@ -18,12 +19,12 @@ class SyncFilter(Node):
     def __init__(self):
         super().__init__('sync_filter')
 
-        self.sync_publisher_ = self.create_publisher(SyncMsg, '/imu_dvl_synchronized_data', 10)
+        self.sync_publisher_ = self.create_publisher(SyncMsg, '/IDD_synchronized_data', 10)
         self.last_imu_sync_ts_sec = math.nan
 
         self.dvl_sub = Subscriber(self, Twist, '/dvl/velocity')
         self.imu_sub = Subscriber(self, Imu, '/imu_synchronized_data')
-        self.depth_sub = Subscriber(self, Float64, '/depth_sensor/depth_meter')
+        self.depth_sub = Subscriber(self, Depth, '/depth/data')
 
         # TODO: ALSO SUBSCRIBE TO ALL OTHER CRITICAL IMU MESSAGES
 
@@ -49,43 +50,45 @@ class SyncFilter(Node):
         )
         self.ts_caseC.registerCallback(self.sync_callback_C)
 
+        self.get_logger().info(f'Running IMU/DVL/Depth data synchronization')
+
     def sync_callback_A(self, dvl_msg, imu_msg, depth_msg):
         self.last_imu_sync_ts_sec = imu_msg.header.stamp.sec # update most recent IMU time for valid full sync
 
         sync_msg = SyncMsg()
-        sync_msg.dvl_available = True
+        sync_msg.dvl_available = Bool(data=True)
         sync_msg.dvl_data = dvl_msg
 
-        sync_msg.imu_available = True
+        sync_msg.imu_available = Bool(data=True)
         sync_msg.imu_data = imu_msg
 
-        sync_msg.depth_available = True
+        sync_msg.depth_available = Bool(data=True)
         sync_msg.depth_data = depth_msg
         
         self.sync_publisher_.publish(sync_msg)
-        self.get_logger().info(f'Publishing fully synchronized data: {sync_msg}')
+        # self.get_logger().info(f'Publishing fully synchronized data: {sync_msg}')
 
     def sync_callback_B(self, dvl_msg, imu_msg):
         CurrTS = imu_msg.header.stamp.sec 
         if CurrTS != self.last_imu_sync_ts_sec: # if time is the same as full sync, ignore since we've already pub'd
             sync_msg = SyncMsg()
-            sync_msg.dvl_available = True
+            sync_msg.dvl_available = Bool(data=True)
             sync_msg.dvl_data = dvl_msg
 
-            sync_msg.imu_available = True
+            sync_msg.imu_available = Bool(data=True)
             sync_msg.imu_data = imu_msg
 
-            sync_msg.depth_available = False
+            sync_msg.depth_available = Bool(data=False)
             sync_msg.depth_data = math.nan
             
             self.sync_publisher_.publish(sync_msg)
-            self.get_logger().info(f'Publishing partially synchronized data: {sync_msg}')
+            # self.get_logger().info(f'Publishing partially synchronized data: {sync_msg}')
 
     def sync_callback_C(self, imu_msg, depth_msg):
         CurrTS = imu_msg.header.stamp.sec
         if CurrTS != self.last_imu_sync_ts_sec: # if time is the same as full sync, ignore since we've already pub'd
             sync_msg = SyncMsg()
-            sync_msg.dvl_available = False
+            sync_msg.dvl_available = Bool(data=False)
 
             dvl_msg = Twist()
             dvl_msg.linear.x = math.nan
@@ -96,14 +99,14 @@ class SyncFilter(Node):
             dvl_msg.angular.z = math.nan
             sync_msg.dvl_data = dvl_msg
 
-            sync_msg.imu_available = True
+            sync_msg.imu_available = Bool(data=True)
             sync_msg.imu_data = imu_msg
 
-            sync_msg.depth_available = True
-            sync_msg.depth_data = depth_msg
+            sync_msg.depth_available = Bool(data=True)
+            sync_msg.depth_data = depth_msg.depth
             
             self.sync_publisher_.publish(sync_msg)
-            self.get_logger().info(f'Publishing partially synchronized data: {sync_msg}')
+            # self.get_logger().info(f'Publishing partially synchronized data: {sync_msg}')
 
 
 def main(args=None):
