517e3b841ac352234108a60b23f20fc346453d6d
selenasun1618 <selenasun02@gmail.com>
2024-07-16 23:39:27 -0700

    modified waypoint switching to have 2 epsilons

--- src/guidance/guidance/waypoints.py
+++ src/guidance/guidance/waypoints.py
@@ -14,8 +14,7 @@ class CheckpointManager(Node):
     def __init__(self):
         super().__init__("waypoints")
 
-        # waypoints = [[0, 0, 0, 0, 0, 0]]
-        with open('/home/selenas/SAUV/SAUV-Autonomy/src/guidance/data/waypoints.json', 'r') as f:
+        with open('/home/selenas/SAUV/SAUV-Autonomy/src/guidance/data/waypoints.json', 'r') as f: # orin filepath
             waypoints = json.load(f)
         self._checkpoints = np.array(waypoints, dtype=np.float64)
         self._checkpoints_index = 0
@@ -45,7 +44,7 @@ class CheckpointManager(Node):
         )
         # msg.data = pwm
         self._desired_pose_pub.publish(msg)
-        self.get_logger().info(f"Published desired_pose:\n[{msg.position.x}, {msg.position.y}, {msg.position.z}, {msg.orientation.x}, {msg.orientation.y}, {msg.orientation.z}]")
+        # self.get_logger().info(f"Published desired_pose:\n[{msg.position.x}, {msg.position.y}, {msg.position.z}, {msg.orientation.x}, {msg.orientation.y}, {msg.orientation.z}]")
 
     def current_state_callback(self, msg: State):
         current_pose = np.array(
@@ -58,13 +57,19 @@ class CheckpointManager(Node):
                 msg.orientation.z,
             ]
         )
-        print(f"TODO: change back error")
-        print(f"cur: {current_pose[0]}\ndesired:{self._desired_pose[0]}")
-        # TODO CHANGE BACK ERROR
-        if current_pose[0] - self._desired_pose[0] < 0.3:
-            self._desired_pose = self._checkpoints[self._checkpoints_index]
+        eps_position = 0.3 # TODO tune
+        eps_angle = 0.2 # TODO tune
+
+        position_error = np.linalg.norm(current_pose[:3] - self._desired_pose[:3])
+        angle_error = np.dot(current_pose[3:], self._desired_pose[3:]) / (np.linalg.norm(current_pose[3:]) * np.linalg.norm(self._desired_pose[3:])) # cosine similarity
+        self.get_logger().info(f"Position Error: {position_error}\nAngle Error: {angle_error}")
+        x_error = current_pose[0] - self._desired_pose[0] # for specific waypoints
+
+        # if x_error < eps_position:
+        if position_error < eps_position and angle_error < eps_angle:
             if self._checkpoints_index < len(self._checkpoints) - 1:
                 self._checkpoints_index += 1
+                self._desired_pose = self._checkpoints[self._checkpoints_index]
 
 
 def main(args=None):
