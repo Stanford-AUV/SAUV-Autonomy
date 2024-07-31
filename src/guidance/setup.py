8b8aec6dd2de2cbcddc6e98ab1ec866339ff63b0
selenasun1618 <selenasun02@gmail.com>
2024-07-31 00:11:13 -0700

    waypoints -> manual_waypoints

--- src/guidance/setup.py
+++ src/guidance/setup.py
@@ -18,6 +18,6 @@ setup(
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": ["waypoints = guidance.waypoints:main"],
+        "console_scripts": ["manual_waypoints = guidance.manual_waypoints:main"],
     },
 )
