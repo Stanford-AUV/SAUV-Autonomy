6f0ca1309a6ac5c31e65a6f33a6677707e095073
Caleb Weaver <calebbweaver@gmail.com>
2024-08-04 15:47:07 -0700

    Squashed commit of the following:
    commit 877326209c197191c0d8e1cb369f17784635a10f
    Author: Caleb Weaver <calebbweaver@gmail.com>
    Date:   Sun Aug 4 15:46:56 2024 -0700
    
        Squashed commit of the following:
    
        commit 9a604ef4f7bb1e1fa5be99b253b6e78144f50150
        Author: selenasun1618 <selenasun02@gmail.com>
        Date:   Sat Aug 3 17:19:16 2024 -0700
    
            fixed bug in manual_waypoints
    
        commit 882fe6217898f013fc932b8c6cdd1fc770fe611f
        Author: Miyu Kojima <miyuk@stanford.edu>
        Date:   Sat Aug 3 17:18:20 2024 -0700
    
            added xspublic folder
    
        commit 23238a589bf4d0e730a5ecb41b4d5e182ce4e00d
        Author: Miyu Kojima <miyuk@stanford.edu>
        Date:   Sat Aug 3 17:16:58 2024 -0700
    
            updated gitignore to allow lib/xspublic
    
        commit ea3b90af2d66d839321dda54f7a8c47910efa874
        Author: selenasun1618 <selenasun02@gmail.com>
        Date:   Fri Aug 2 23:02:15 2024 -0700
    
            fixes for State.msg -> Odometry.msg in manual_waypoints.py
    
        commit 55076c334ecd348684648fec5d813122cabaa3af
        Author: selenasun1618 <selenasun02@gmail.com>
        Date:   Fri Aug 2 21:45:27 2024 -0700
    
            aug 2 rosbags
    
        commit 94149a80de5dcda088fd4ba0ba4ef6f25f369205
        Author: selenasun1618 <selenasun02@gmail.com>
        Date:   Fri Aug 2 20:50:53 2024 -0700
    
            modified controller to read Odometry.msg as robot state
    
        commit e9b49bd0d39dfca71d76b32021fde605dde087e8
        Merge: 8c730cc 316ef81
        Author: Yuanzhe Dong <5069709+yuanzhedong@users.noreply.github.com>
        Date:   Wed Jul 31 17:24:40 2024 -0700
    
            Merge pull request #15 from Stanford-AUV/yz/dev/yolo_node
    
            add perception node
    
        commit 8c730ccf100721062ee9c574e5d73dbc1cc36ca6
        Author: selenasun1618 <selenasun02@gmail.com>
        Date:   Wed Jul 31 00:11:13 2024 -0700
    
            waypoints -> manual_waypoints
    
        commit 1c40ca3d9ae7c8193793cc557a5ff6d924cd25af
        Author: Scott Hickmann <scotthickmann21@gmail.com>
        Date:   Tue Jul 30 19:20:57 2024 -0700
    
            Fix paths and sim launch file
    
        commit 316ef814d642c653dbc7e555fd4823a63be3de70
        Author: selenasun1618 <selenasun02@gmail.com>
        Date:   Mon Jul 15 23:57:23 2024 -0700
    
            update code path
    
        commit 51ecd1a800a6cebc9fe0dac1ed264c71446e89e9
        Merge: e704ef2 0e4dc43
        Author: selenasun1618 <selenasun02@gmail.com>
        Date:   Mon Jul 15 23:05:44 2024 -0700
    
            Merge remote-tracking branch 'origin/main' into yz/dev/yolo_node
    
        commit e704ef2014c8037a6f09df3679bbb68a09e05f86
        Author: Yuanzhe Dong <yuanzhedong@gmail.com>
        Date:   Mon Jul 8 00:15:05 2024 -0700
    
            add video and debug node
    
        commit 9917a93ed3d066838594026eeb1fd619806a1a12
        Author: Yuanzhe Dong <yuanzhedong@gmail.com>
        Date:   Mon Jul 8 00:14:17 2024 -0700
    
            update topics
    
        commit 6810052715f1c44a1d24cbf68e58410ce9b36512
        Author: Yuanzhe Dong <yuanzhedong@gmail.com>
        Date:   Tue Jul 2 22:27:38 2024 -0700
    
            code cleanup
    
        commit fb89a762dd71d0c843488a1200c55f528bb4387f
        Author: Yuanzhe Dong <yuanzhedong@gmail.com>
        Date:   Tue Jul 2 21:54:38 2024 -0700
    
            fix msg package
    
        commit 9587fbf5f8428076a808a0969f715ab3f5549251
        Author: Yuanzhe Dong <yuanzhedong@gmail.com>
        Date:   Tue Jul 2 21:52:10 2024 -0700
    
            add perception node

--- src/control/utils.py
+++ src/control/utils.py
@@ -1,7 +1,9 @@
 import numpy as np
+import math
 from geometry_msgs.msg import Vector3, Quaternion, Point
+from nav_msgs.msg import Odometry
 from msgs.msg import Pose, State, Wrench
-
+from scipy.spatial.transform import Rotation
 
 def to_np(o) -> np.ndarray:
     """Converts a ROS message to a numpy array."""
@@ -29,6 +31,21 @@ def pose_to_np(o: State | Pose):
             ]
         )
 
+def quaternion_to_euler(quaternion):
+    """
+    Convert a quaternion into Euler angles (roll, pitch, yaw) in radians
+    """
+    rot = Rotation.from_quat(quaternion)
+    return rot.as_euler('xyz')
+    
+
+def euler_to_quaternion(euler_angle):
+    """
+    Convert Euler angles (roll, pitch, yaw) in radians to quaternions
+    """
+    rot = Rotation.from_euler(euler_angle)
+    return rot.as_quat()
+
 
 def state_to_np(o: State | Pose):
     if isinstance(o, State) or isinstance(o, Pose) or isinstance(o, Wrench):
@@ -41,6 +58,24 @@ def state_to_np(o: State | Pose):
                 o.orientation_euler.y,
                 o.orientation_euler.z,
             ]
+        )
+    
+def odometry_to_np(o: Odometry):
+    if isinstance(o, Odometry):
+        position = [
+            o.pose.pose.position.x,
+            o.pose.pose.position.y,
+            o.pose.pose.position.z
+        ]
+        orientation_quat = [
+            o.pose.pose.orientation.x,
+            o.pose.pose.orientation.y,
+            o.pose.pose.orientation.z,
+            o.pose.pose.orientation.w
+        ]
+        orientation_euler = quaternion_to_euler(orientation_quat)
+        return np.array(
+            position.extend(orientation_euler)
         )
 
 
---
+++ src/perception/perception/perception_node.py
@@ -0,0 +1,221 @@
+from typing import List, Dict
+
+import rclpy
+from rclpy.qos import QoSProfile
+from rclpy.qos import QoSHistoryPolicy
+from rclpy.qos import QoSDurabilityPolicy
+from rclpy.qos import QoSReliabilityPolicy
+from rclpy.lifecycle import LifecycleNode
+from rclpy.lifecycle import TransitionCallbackReturn
+from rclpy.lifecycle import LifecycleState
+
+from cv_bridge import CvBridge
+
+from torch import cuda
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
+from ultralytics.engine.results import Boxes
+
+from std_srvs.srv import SetBool
+from sensor_msgs.msg import Image
+from yolov8_msgs.msg import Point2D
+from yolov8_msgs.msg import BoundingBox2D
+from yolov8_msgs.msg import Detection
+from yolov8_msgs.msg import DetectionArray
+
+
+class Yolov8Node(LifecycleNode):
+
+    def __init__(self) -> None:
+        super().__init__("yolov8_node")
+
+        # params
+        self.declare_parameter("model", "/home/selenas/SAUV/data/best.pt")
+        self.declare_parameter("device", "cuda")
+        self.declare_parameter("threshold", 0.5)
+        self.declare_parameter("enable", True)
+        self.declare_parameter("image_reliability",
+                               QoSReliabilityPolicy.BEST_EFFORT)
+
+        self.get_logger().info("Yolov8 Node created")
+
+    def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
+        self.get_logger().info(f"Configuring {self.get_name()}")
+
+
+        self.model = self.get_parameter(
+            "model").get_parameter_value().string_value
+
+        self.device = self.get_parameter(
+            "device").get_parameter_value().string_value
+
+        self.threshold = self.get_parameter(
+            "threshold").get_parameter_value().double_value
+
+        self.enable = self.get_parameter(
+            "enable").get_parameter_value().bool_value
+
+        self.reliability = self.get_parameter(
+            "image_reliability").get_parameter_value().integer_value
+
+        self.image_qos_profile = QoSProfile(
+            reliability=self.reliability,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            durability=QoSDurabilityPolicy.VOLATILE,
+            depth=1
+        )
+
+        self._pub = self.create_lifecycle_publisher(
+            DetectionArray, "detections", 10)
+        self._srv = self.create_service(
+            SetBool, "enable", self.enable_cb
+        )
+        self.cv_bridge = CvBridge()
+
+        print("@@@@@@@@@@@2")
+        #import torch
+        #print(torch.cuda.is_available())
+
+        print("#####")
+        return TransitionCallbackReturn.SUCCESS
+
+    def enable_cb(self, request, response):
+        self.enable = request.data
+        response.success = True
+        return response
+
+    def on_activate(self, state: LifecycleState) -> TransitionCallbackReturn:
+        self.get_logger().info(f"Activating {self.get_name()}")
+
+        self.yolo = YOLO(self.model)
+
+        if "v10" not in self.model:
+            self.yolo.fuse()
+
+        # subs
+        self._sub = self.create_subscription(
+            Image,
+            "oak/rgb/image_raw",
+            self.image_cb,
+            self.image_qos_profile
+        )
+
+        super().on_activate(state)
+
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_deactivate(self, state: LifecycleState) -> TransitionCallbackReturn:
+        self.get_logger().info(f"Deactivating {self.get_name()}")
+
+        del self.yolo
+        if "cuda" in self.device:
+            self.get_logger().info("Clearing CUDA cache")
+            cuda.empty_cache()
+
+        self.destroy_subscription(self._sub)
+        self._sub = None
+
+        super().on_deactivate(state)
+
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_cleanup(self, state: LifecycleState) -> TransitionCallbackReturn:
+        self.get_logger().info(f"Cleaning up {self.get_name()}")
+
+        self.destroy_publisher(self._pub)
+
+        del self.image_qos_profile
+
+        return TransitionCallbackReturn.SUCCESS
+
+    def parse_hypothesis(self, results: Results) -> List[Dict]:
+
+        hypothesis_list = []
+
+        box_data: Boxes
+        for box_data in results.boxes:
+            hypothesis = {
+                "class_id": int(box_data.cls),
+                "class_name": self.yolo.names[int(box_data.cls)],
+                "score": float(box_data.conf)
+            }
+            hypothesis_list.append(hypothesis)
+
+        return hypothesis_list
+
+    def parse_boxes(self, results: Results) -> List[BoundingBox2D]:
+
+        boxes_list = []
+
+        box_data: Boxes
+        for box_data in results.boxes:
+
+            msg = BoundingBox2D()
+
+            # get boxes values
+            box = box_data.xywh[0]
+            msg.center.position.x = float(box[0])
+            msg.center.position.y = float(box[1])
+            msg.size.x = float(box[2])
+            msg.size.y = float(box[3])
+
+            # append msg
+            boxes_list.append(msg)
+
+        return boxes_list
+
+    def image_cb(self, msg: Image) -> None:
+
+        if self.enable:
+
+            # convert image + predict
+            cv_image = self.cv_bridge.imgmsg_to_cv2(msg)
+            results = self.yolo.predict(
+                source=cv_image,
+                verbose=False,
+                stream=False,
+                conf=self.threshold,
+                device=self.device
+            )
+            results: Results = results[0].cpu()
+
+            if results.boxes:
+                hypothesis = self.parse_hypothesis(results)
+                boxes = self.parse_boxes(results)
+                print("boxes:", boxes)
+
+            else:
+                print("empty")
+            # create detection msgs
+            detections_msg = DetectionArray()
+
+            for i in range(len(results)):
+
+                aux_msg = Detection()
+
+                if results.boxes:
+                    aux_msg.class_id = hypothesis[i]["class_id"]
+                    aux_msg.class_name = hypothesis[i]["class_name"]
+                    aux_msg.score = hypothesis[i]["score"]
+
+                    aux_msg.bbox = boxes[i]
+
+                detections_msg.detections.append(aux_msg)
+
+            # publish detections
+            detections_msg.header = msg.header
+            self._pub.publish(detections_msg)
+
+            del results
+            del cv_image
+
+
+def main():
+    rclpy.init()
+    node = Yolov8Node()
+    node.trigger_configure()
+    node.trigger_activate()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
