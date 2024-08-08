a81ad1596c7242ca7f90246539ef7a98adffda4d
selenasun1618 <selenasun02@gmail.com>
2024-08-08 00:01:50 -0700

    added install req

--- src/control/pwm_tester.py
+++ src/control/pwm_tester.py
@@ -1,7 +1,7 @@
 import numpy as np
 import serial
 import time
-from control.force_to_pwm import total_force_to_individual_thrusts, thrusts_to_pwm
+from control.force_to_pwm import total_force_to_individual_thrusts, all_thrusts_to_pwm
 
 
 def main():
@@ -9,23 +9,22 @@ def main():
     thruster_ids = [f"thruster{i}" for i in range(1, 9)]
     testing_wrench = np.array([0, -0.0, 0, 0, 0, 0])
     thrusts = total_force_to_individual_thrusts(testing_wrench)
-    pwms = thrusts_to_pwm(thrusts)
-    # pwms = [1500, 1500, 1500, 1500, 1450, 1550, 1450, 1550] # specify pwms here
+    # pwms = all_thrusts_to_pwm(thrusts)
+    pwms = [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500] # specify pwms here
     print(f"pwms: {pwms}")
 
-    n = 20
+    n = 50
     # for i in range(8):
     #   time.sleep(10)
     #   print(f"{80 - i * 10} seconds left!")
     for j in range(n):
-        time.sleep(5)
         for i, thruster in enumerate(thruster_ids):
             if j == n - 1:
                 command = f"{i+2} 1500\n"
             else:
                 command = f"{i+2} {pwms[i]}\n"
             portName.write(command.encode())
-            ##time.sleep(.1)
+            time.sleep(.1)
 
 
 if __name__ == "__main__":
--- src/perception/setup.py
+++ src/perception/setup.py
@@ -19,7 +19,6 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            f"oakd = {package_name}.oakd:main",
             f"data_node = {package_name}.data_node:main",
             f"perception_node = {package_name}.perception_node:main",
             f"debug_node = {package_name}.debug_node:main",
